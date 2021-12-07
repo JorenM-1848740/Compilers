679d47cab0d144a0ea070c29c5c8f2d20f1ba519
JorenM-1848740 <joren.michels@student.uhasselt.be>
2021-12-07 20:49:57 +0100

    else in flex

--- testMain.go
+++ testMain.go
@@ -1,5 +1,18 @@
 package main;
 
+func fib(n int) (int) {
+	var a bool = n == 0;
+	var b bool = n == 1;
+	var c bool = a || b;
+	if (c) {
+		return 1;
+	} else {
+		var left int = fib(n - 1);
+		var right int = fib(n - 2);
+		return left + right;
+	}
+};
+
 func main() {
-	
-}
+	print(fib(8));
+};
