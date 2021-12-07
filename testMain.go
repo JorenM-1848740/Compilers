bf1549ea2ec5c700195dac83474cb0061105fbe2
JorenM-1848740 <joren.michels@student.uhasselt.be>
2021-12-07 20:56:50 +0100

    comma in lex

--- testMain.go
+++ testMain.go
@@ -1,18 +1,39 @@
 package main;
 
-func fib(n int) (int) {
-	var a bool = n == 0;
-	var b bool = n == 1;
-	var c bool = a || b;
-	if (c) {
-		return 1;
-	} else {
-		var left int = fib(n - 1);
-		var right int = fib(n - 2);
-		return left + right;
-	}
-};
+var a int = 0;
+var z int = 10;
+var hex int = 0;
+var bin int = 0;
+var oct int = 0;
+var boolean bool = true;
+var sum int = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19;
+var andtf bool = true && false;
+var ortf bool = true || false;
+var bruh int = 5;
 
 func main() {
-	print(fib(8));
+	var b int = 5;
+	var c int = a + b;
+
+	print(a, b, c, z);
+	
+	var a1, z1 int = a, z;
+	print(a1, z1);
+	a1++;
+	z1++
+	print(a1, z1);
+	print(true, false);
+
+	if (true) {
+		var abc int = 123;
+		abc++;
+		print(abc);
+	}
+
+	test();
+	print(bruh);
+}
+
+func test() {
+	bruh++;
 };
