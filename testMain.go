105086ef95f20cd77b6a50f06aeb1b1fd79fbfd3
JorenM-1848740 <joren.michels@student.uhasselt.be>
2022-08-05 11:09:43 +0200

    ReturnStmt

--- testMain.go
+++ testMain.go
@@ -6,41 +6,23 @@ var c bool = true || false;
 
 func main(a int) (b int){
     var n int;
-    var t1 = 0
+    var t1 = 4 +6
     var ()
     var aaa int;
     var t2 = 1
-    var nextTerm = 0
-
-    {
-        var xx int = 5;
-    }
-
-    xx = 5;
     
-    for  {
-        if i==1{
-
-        }
-        if i==2{
-
-        }
-        nextTerm = t1 && t2
-        t1=t2
-        return 5;
-        t2=nextTerm
-    }
-
-        var ljl int;
-
-        {
-            return 5;
-
-        }
+    return 5;
 
     
 }
 
-func testing(d int, e int) (a bool, b bool){
-    return 5;
+var n = 5;
+
+func testing(d int, e int) (int, int){
+
+    return testing2();
+}
+
+func testing2()(a int, b int){
+    return;
 }
--- treeNodes/returnStmt.cpp
+++ treeNodes/returnStmt.cpp
@@ -1,4 +1,5 @@
 #include "../absyn.hpp"
+#include "../treeClasses/scope.hpp"
 
 ReturnStmt_::ReturnStmt_(){
 
@@ -16,5 +17,98 @@ void ReturnStmt_::print(int d){
 }
 
 void ReturnStmt_::typeCheck(vector<Scope>& scopeStack, vector<string>& typeErrors){
+
+    string currentFunctionName = scopeStack[0]->getCurrentFunctionTypeChecking();
+    Signature currentFunctionSignature = scopeStack[0]->getFunctionSignature(currentFunctionName);
+    vector<std::pair<string, string>> signatureResults = currentFunctionSignature->getResults();
+
+    //If there is no result in the signature
+    if (signatureResults.size() == 0){
+        if (expressionList != nullptr){
+            typeErrors.push_back("Too many return values!");
+        }
+        return;
+    }
+    //If the expression list is empty (check for named results in signature)
+    else if (expressionList == nullptr){
+
+        vector<string> allIdentifiersResults;
+        for (int i = 0; i < signatureResults.size();++i){
+            allIdentifiersResults.push_back(signatureResults[i].first);
+        }
+
+        bool namedResults = true;
+        bool unnamedResults = true;
+
+        for (int i = 0; i < allIdentifiersResults.size();++i){
+            if (allIdentifiersResults[i] != ""){
+                unnamedResults = false;
+            }
+            if (allIdentifiersResults[i] == ""){
+                namedResults = false;
+            }
+        }
+
+        if (!namedResults){
+            typeErrors.push_back("Empty return statement only allowed when all result types are named!");
+            return;
+        }
+    }
+    else{
+        vector<vector<string>> returnTypes;
+        expressionList->getTypes(scopeStack, typeErrors, returnTypes);
+
+        bool allSingleValued = true;
+        for (int i = 0; i < returnTypes.size();++i){
+            if (returnTypes[i].size() != 1){
+                allSingleValued = false;
+                break;
+            }
+        }
+        bool singleMultiValued = false;
+        if (returnTypes.size() == 1 && returnTypes[0].size() > 1){
+            singleMultiValued = true;
+        }
+
+        //If single valued return values
+        if (allSingleValued){
+            //If return values are not the same size as signature results
+            if (returnTypes.size() != signatureResults.size()){
+                typeErrors.push_back("Return values not same size as signature results!");
+                return;
+            }
+            else{
+                for (int i = 0; i < returnTypes.size();++i){
+                    //If types don't correspond
+                    if (returnTypes[i][0] != signatureResults[i].second){
+                        typeErrors.push_back("Return values not the same type as signature results!");
+                        return;
+                    }
+                }
+            }
+        }
+        //If one multi valued return value
+        else if (singleMultiValued){
+            if (returnTypes[0].size() != signatureResults.size()){
+                typeErrors.push_back("Return values not same size as signature results!");
+                return;
+            }
+            else{
+                for (int i = 0; i < returnTypes[0].size();++i){
+                    //If types don't correspond
+                    if (returnTypes[0][i] != signatureResults[i].second){
+                        typeErrors.push_back("Return values not the same type as signature results!");
+                        return;
+                    }
+                }
+            }
+        }
+        //Return values formated wrongly
+        else{
+            typeErrors.push_back("Incorrect return values!");
+        }
+    }
+
     
+
 }
